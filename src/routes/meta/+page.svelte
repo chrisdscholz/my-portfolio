332c4bd69b69bfe45e45f784634a497b453ec39f
chrisdscholz <153697137+chrisdscholz@users.noreply.github.com>
2024-11-14 22:33:44 -0800

    update

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,2 +1,24 @@
+<script>
+    import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+
+    let data = []
+
+    onMount(async () => {
+        data = await d3.csv('loc.csv', (row) => ({
+            ...row,
+            line: Number(row.line),
+            depth: Number(row.depth),
+            length: Number(row.length),
+            date: new Date(row.date + 'T00:00' + row.timezone),
+            datetime: new Date(row.datetime),
+        }));
+    });
+
+    console.log(data)
+</script>
+
 <h1>Meta</h1>
 <p>This page includes stats about the code of this website</p>
+
+<p>Total lines of code: {data.length}</p>
