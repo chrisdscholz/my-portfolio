283a341d951bc6d75a910d60679a5f5ae0e9c7d5
chrisdscholz <153697137+chrisdscholz@users.noreply.github.com>
2024-11-15 20:47:49 -0800

    update

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -13,6 +13,25 @@
 
     let files = [];
 
+    let dateRange = [];
+
+    let width = 1000,
+        height = 600;
+
+    let margin = { top: 10, right: 10, bottom: 30, left: 20};
+
+    let usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+    };
+
+    usableArea.width = usableArea.right - usableArea.left;
+    usableArea.height = usableArea.bottom - usableArea.top;
+
+    let yAxisGridlines;
+
     onMount(async () => {
         data = await d3.csv('loc.csv', (row) => ({
             ...row,
@@ -30,7 +49,7 @@
                 let { author, date, time, timezone, datetime } = first;
                 let ret = {
                     id: commit,
-                    url: 'https://github.com/chrisdscholz/my-portfolio/commit' + commit,
+                    url: 'https://github.com/chrisdscholz/my-portfolio/commit/' + commit,
                     author,
                     date,
                     time,
@@ -54,29 +73,45 @@
         maxD = d3.max(data, d => d.depth);
         avgD = d3.mean(data, d => d.depth);
         files = d3.group(data, d => d.file);
+        dateRange = d3.extent(commits, d => d.datetime);
     });
 
-    let dateRange = d3.extent(commits, d => d.datetime);
-
-    // minDate = d3.extent(date)
-
-    let width = 1000,
-        height = 600;
-
-    const yScale = d3.scaleLinear()
+    $: yScale = d3.scaleLinear()
         .domain([0, 24])
-        .range([0, height]);
+        .range([usableArea.bottom, usableArea.top]);
     
-    const xScale = d3.scaleUtc()
-        .domain([dateRange])
-        .range([0, width])
+    $: xScale = d3.scaleUtc()
+        .domain([dateRange[0], dateRange[1]])
+        .range([usableArea.left, usableArea.right])
         .nice();
+
+    let xAxis, yAxis;
+
+    $: {
+        d3.select(xAxis).call(d3.axisBottom(xScale));
+        d3.select(yAxis).call(
+            d3
+            .axisLeft(yScale)
+        .tickFormat((d) => String(d % 24).padStart(2, 0) + ':00')
+        );
+    }
+
+    $: {
+        d3.select(yAxisGridlines).call(
+            d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width),
+        );
+    }
+
+    let hoveredIndex = -1;
+    $: hoveredCommit = commits[hoveredIndex] ?? hoveredCommit ?? {};
+
+    let cursor = { x: 0, y: 0 };
 </script>
 
 <h1>Meta</h1>
 <p>This page includes stats about the code of this website</p>
 
-<p>Total lines of code: {data.length}</p>
+<p>{JSON.stringify(cursor, null, "\t")}</p>
 
 <h2>Codebase stats</h2>
 <section class="code_stats">
@@ -99,6 +134,14 @@
 </section>
 
 <svg viewBox="0 0 {width} {height}">
+    <g 
+        class="gridlines"
+        transform="translate({usableArea.left}, 0)"
+        bind:this="{yAxisGridlines}"
+    
+    />
+    <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+    <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
     <g class="dots">
         {#each commits as commit, index}
         <circle
@@ -108,15 +151,84 @@
         cy={
         yScale(commit.hourFrac)
         }
-        r="5"
+        r="7"
         fill="steelblue"
+        on:mouseenter={evt => {
+            hoveredIndex = index;
+            cursor = {x: evt.x, y: evt.y};
+        }} 
+        on:mouseleave={evt =>
+            hoveredIndex = -1}
         />
         {/each}
     </g>
+
+    <!-- <dl id="commit-tooltip" class="info tooltip">
+        <dt>Commit</dt>
+        <dd>
+            <a href="{ hoveredCommit.url }" target="_blank">{ hoveredCommit.id }</a>
+        </dd>
+
+        <dt>Date</dt>
+        <dd>{ hoveredCommit.datetime?.toLocaleString("en", {dateStyle: "full"}) }</dd>
+    </dl> -->
 </svg>
+
+<dl id="commit-tooltip" class="info tooltip" hidden={hoveredIndex === -1} style="top: {cursor.y}px; left: {cursor.x}px">
+    <dt>Commit ID</dt>
+    <dd>
+        <a href="{ hoveredCommit.url }" target="_blank">{ hoveredCommit.id }</a>
+    </dd>
+
+    <dt>Author</dt>
+    <dd>{ hoveredCommit.author }</dd>
+
+    <dt>Date</dt>
+    <dd>{ hoveredCommit.datetime?.toLocaleString("en", {dateStyle: "full"}) }</dd>
+
+    <dt>Time</dt>
+    <dd>{ hoveredCommit.datetime?.toLocaleString("en", {timeStyle: "short"}) }</dd>
+</dl>
 
 <style>
     svg{
         overflow: visible;
+    }
+
+    .gridlines {
+        stroke-opacity: 0.2;
+    }
+
+    dl.info {
+        display: grid;
+        margin: 0;
+        transition-duration: 500ms;
+        transition-property: opacity, visibility;
+
+        &[hidden]:not(:hover, :focus-within) {
+            opacity: 0;
+            visibility: hidden;
+        }
+    }
+
+    .tooltip {
+        position: fixed;
+        top: 1em;
+        left: 1em;
+        background-color: oklch(40% 0% 0 / 10%);
+        /* box-shadow: 2px 2px 1px gray; */
+        border-radius: 2px;
+        border-color: gray;
+        backdrop-filter: blur(10px);
+    }
+
+    circle {
+        transition: 200ms;
+
+        &:hover {
+            transform: scale(1.5);
+        }
+        transform-origin: center;
+        transform-box: fill-box;
     }
 </style>
